36bcc62f3bef91ce1373011dc5767187cfa8efdd
Yterix-strona <148962522+Yterix-strona@users.noreply.github.com>
2025-12-30 20:45:21 +0100

    index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Moja Strona</title>
+    <style>
+        body {
+            background-color: #222;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+            margin: 0;
+        }
+        img {
+            width: 300px; /* możesz zmienić rozmiar */
+            border-radius: 15px;
+        }
+    </style>
+</head>
+<body>
+    <img src="profile.jpeg" alt="Moje zdjęcie">
+</body>
+</html>
